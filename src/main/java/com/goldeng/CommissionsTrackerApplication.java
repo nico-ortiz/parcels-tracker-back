8f6e3fa4aba91a1cae8faf502c9ced82f25c4d8d
nico-ortiz <nicortiz1234@gmail.com>
2024-05-29 18:09:21 -0300

    initial commit

---
+++ src/main/java/com/goldeng/CommissionsTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.goldeng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommissionsTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommissionsTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/goldeng/CommissionsTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goldeng;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommissionsTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

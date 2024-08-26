46aae20582cd4b672ae2d4c9f8b1df6cf86e751c
nico-ortiz <nicortiz1234@gmail.com>
2024-08-26 18:11:17 -0300

    add cross origin anotation

--- src/main/java/com/goldeng/controller/ParcelController.java
+++ src/main/java/com/goldeng/controller/ParcelController.java
@@ -3,6 +3,7 @@ package com.goldeng.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.goldeng.dto.ParcelDTO;
 import com.goldeng.service.IParcelService;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/parcels")
 public class ParcelController {
